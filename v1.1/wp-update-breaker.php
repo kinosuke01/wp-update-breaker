dd97581e317d961b03b31048fc3e5e4945f31420
kinosuke01 <kinosuke01@pepabo.com>
2024-09-17 18:21:28 +0900

    update v1.1

--- v1.1/wp-update-breaker.php
+++ v1.1/wp-update-breaker.php
@@ -2,9 +2,13 @@
 /*
 Plugin Name: WP Update Breaker
 Description: This plugin breaks the site display when updating. Used for testing.
-Version: 1.1
+Version: 1.0
 Author: kinosuke01
+Plugin URI: https://kinosuke01.github.io/wp-update-breaker/
+Update URI: https://kinosuke01.github.io/wp-update-breaker/info.json
 */
+
+require_once 'updater.php';
 
 add_filter('the_content', function($content) {
     $text = <<<'EOT'
